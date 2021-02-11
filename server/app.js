c67a8d91a53f6af5faa2540b9456ac8687e97a19
Aaron Ojala <aaronojala@yahoo.com>
2021-02-11 22:42:17 +0200

    start the simple full stack app

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const port = 5000;
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv')
+dotenv.config();
+
+const dbService = require('./dbService')
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+
+// create
+app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ success: true }))
+    .catch(err => console.log(err));
+
+});
+
+// read
+app.get('/getAll', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+// update
+
+
+// delete
+
+app.listen(port, () => console.log('app is running'));
